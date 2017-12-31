30761b1137139685bbf1fd0cb7a09d09cb690fba
yassu <yassu0320.dev@gmail.com>
2018-01-01 01:02:46 +0900

    Quaternion.-

--- src/test/scala/core/number/quaternion.scala
+++ src/test/scala/core/number/quaternion.scala
@@ -19,6 +19,11 @@ class QuaternionSpec extends org.specs2.mutable.Specification {
     val q2 = new Quaternion(5.0, 6.0, 7.0, 8.0)
     q1 + q2 must_== new Quaternion(6.0, 8.0, 10.0, 12.0)
   }
+  "#-" >> {
+    val q1 = new Quaternion(2.0, 3.0, 5.0, 7.0)
+    val q2 = new Quaternion(13.0, 17.0, 19.0, 23.0)
+    q1 - q2 must_== new Quaternion(-11.0, -14.0, -14.0, -16.0)
+  }
   "#toString" >> {
     val q = new Quaternion(1.0, 2.0, 3.0, 4.0)
     q.toString must_== "1.0 + 2.0 i + 3.0 j + 4.0 k"
