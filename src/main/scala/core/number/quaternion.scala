737b114d39d8500c9090c54b0b14c46f9da18680
yassu <yassu0320.dev@gmail.com>
2018-01-01 00:47:00 +0900

    Quaternion.+

--- src/main/scala/core/number/quaternion.scala
+++ src/main/scala/core/number/quaternion.scala
@@ -2,4 +2,10 @@ package mathlib.core.number
 
 case class Quaternion(x0: Double, x1: Double, x2: Double, x3: Double) {
   def this(x0: Double) = this(x0, 0, 0, 0)
+  def +(that: Quaternion) = new Quaternion(
+    x0 + that.x0,
+    x1 + that.x1,
+    x2 + that.x2,
+    x3 + that.x3
+  )
 }
--- src/test/scala/core/number/quaternion.scala
+++ src/test/scala/core/number/quaternion.scala
@@ -14,4 +14,9 @@ class QuaternionSpec extends org.specs2.mutable.Specification {
       q must_== new Quaternion(1.0, 0.0, 0.0, 0.0)
     }
   }
+  "#+" >> {
+    val q1 = new Quaternion(1.0, 2.0, 3.0, 4.0)
+    val q2 = new Quaternion(5.0, 6.0, 7.0, 8.0)
+    q1 + q2 must_== new Quaternion(6.0, 8.0, 10.0, 12.0)
+  }
 }
