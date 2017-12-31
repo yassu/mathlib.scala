b8999a0de09fa373a7d5a5ef193e4fa96b0fa7c4
yassu <yassu0320.dev@gmail.com>
2018-01-01 03:17:18 +0900

    refactoring

--- src/main/scala/core/number/quaternion.scala
+++ src/main/scala/core/number/quaternion.scala
@@ -18,44 +18,17 @@ case class Quaternion(x0: Double, x1: Double, x2: Double, x3: Double) {
     x2 + that.x2,
     x3 + that.x3
   )
-  def +(y: Double) = new Quaternion(
-    x0 + y,
-    x1,
-    x2,
-    x3
-  )
-  def -(that: Quaternion) = new Quaternion(
-    x0 - that.x0,
-    x1 - that.x1,
-    x2 - that.x2,
-    x3 - that.x3
-  )
-  def -(y: Double) = new Quaternion(
-    x0 - y,
-    x1,
-    x2,
-    x3
-  )
-  def *(that: Quaternion) = new Quaternion(
+  def +(y: Double):Quaternion = this + new Quaternion(y, 0, 0, 0)
+  def -(that: Quaternion):Quaternion = this + (-that)
+  def -(y: Double):Quaternion = this + (-y)
+  def *(that: Quaternion):Quaternion = new Quaternion(
     this.x0 * that.x0 - this.x1 * that.x1 - this.x2 * that.x2 - this.x3 * that.x3,
     this.x0 * that.x1 + this.x1 * that.x0 + this.x2 * that.x3 - this.x3 * that.x2,
     this.x0 * that.x2 - this.x1 * that.x3 + this.x2 * that.x0 + this.x3 * that.x1,
     this.x0 * that.x3 + this.x1 * that.x2 - this.x2 * that.x1 + this.x3 * that.x0
   )
-  def *(y: Double):Quaternion = new Quaternion(
-    this.x0 * y,
-    this.x1 * y,
-    this.x2 * y,
-    this.x3 * y
-  )
+  def *(y: Double):Quaternion = this * new Quaternion(y, 0, 0, 0)
   def /(that: Quaternion): Quaternion = this * that.conjugate / Math.pow(that.abs, 2)
-  def /(y: Double): Quaternion = {
-    new Quaternion(
-      this.x0 / y,
-      this.x1 / y,
-      this.x2 / y,
-      this.x3 / y
-    )
-  }
+  def /(y: Double): Quaternion = this * (1 / y)
   override def toString: String = s"$x0 + $x1 i + $x2 j + $x3 k"
 }
