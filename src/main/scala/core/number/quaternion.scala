9f4ff5ee5d321bb15863da71cf450d5a5ddfc27e
yassu <yassu0320.dev@gmail.com>
2018-01-01 02:54:29 +0900

    Quaternion.- for real number

--- src/main/scala/core/number/quaternion.scala
+++ src/main/scala/core/number/quaternion.scala
@@ -28,6 +28,12 @@ case class Quaternion(x0: Double, x1: Double, x2: Double, x3: Double) {
     x2 - that.x2,
     x3 - that.x3
   )
+  def -(y: Double) = new Quaternion(
+    x0 - y,
+    x1,
+    x2,
+    x3
+  )
   def *(that: Quaternion) = new Quaternion(
     this.x0 * that.x0 - this.x1 * that.x1 - this.x2 * that.x2 - this.x3 * that.x3,
     this.x0 * that.x1 + this.x1 * that.x0 + this.x2 * that.x3 - this.x3 * that.x2,
